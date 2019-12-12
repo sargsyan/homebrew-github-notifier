5104b55ee13c704f0fc1aa2ddff9595c9aeaa0b9
sargsyan <serg.sargsyan@gmail.com>
2019-12-12 10:59:21 +0100

    Update github-notifier.rb
    Update the checksum

--- Formula/github-notifier.rb
+++ Formula/github-notifier.rb
@@ -2,7 +2,7 @@ class GithubNotifier < Formula
   desc "Get Github and Github Enterprise notifications in your Mac OS "
   homepage "https://sargsyan.github.io/github-notifier/"
   url "https://github.com/sargsyan/github-notifier/archive/v1.0.3.tar.gz"
-  sha256 "6ae1610c998362217b6eaeb14cf31c09d07ab6e16d3d62a62f359a622f26e1d7"
+  sha256 "49a42a66e22721ef4b346e7dd7504fa7077fbb01e639c2431c43805da422c56d"
 
   depends_on "jq"
 
